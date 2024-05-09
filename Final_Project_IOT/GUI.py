6a9e636bdcce7142a08ae18fe17f99c9983c03f9
Jeremie Beaulieu <jeremie062010@outlook.fr>
2024-05-09 11:34:38 -0400

    changed the UI to show when the garbage is full or not

--- Final_Project_IOT/GUI.py
+++ Final_Project_IOT/GUI.py
@@ -8,12 +8,14 @@ class WasteManagementGUI:
     def __init__(self, master):
         self.master = master
         master.title("Smart Waste Management System")
-        self.act()
+        percentage = 0
 
         # Create label and disabled text field for waste level
         self.waste_level_label = ttk.Label(master, text="Waste Level:")
         self.waste_level_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
-        self.waste_level_text = ttk.Entry(master, state="disabled")
+        self.waste_level_text = ttk.Entry(master)
+        self.waste_level_text.insert(1, '0%')
+        self.waste_level_text.config(state="disabled")
         self.waste_level_text.grid(row=0, column=1, padx=5, pady=5)
 
         # Create label, text field, and button for setting level
@@ -25,12 +27,32 @@ class WasteManagementGUI:
         self.set_level_button.grid(row=1, column=2, padx=5, pady=5)
 
         # Create label for status
-        self.status_label = ttk.Label(master, text="Status: Turned Off", foreground="red")
+        self.status_label = ttk.Label(master, text="Garbage is not full", foreground="green")
         self.status_label.grid(row=2, column=0, columnspan=3, padx=5, pady=5)
+        
+        # Create the unothorized entry label. By default, there is no text so it is "invisible"
+        self.unothorized_label = ttk.Label(master, text="", foreground="red")
+        self.unothorized_label.grid(row=3, column=0, columnspan=3, padx=5, pady=5)
+        
+        # initializing the act method
+        self.act()
         
     def act(self):
         # Call the act method of the interactions instance
         self.interact.act()
+        self.percentage = self.interact.get_percentage()
+        max_waste_level = int(self.waste_level_text.get()[:-1])
+        
+        if self.percentage > max_waste_level:
+            self.status_label.config(text='Garbage is full', foreground="red")
+        else:
+            self.status_label.config(text='Garbage is not full', foreground="green")
+            
+        if self.interact.get_authorization():
+            self.unothorized_label.config(text="UNOTHORIZED ENTRY!")
+        else:
+            self.unothorized_label.config(text="")
+            
         # Schedule the next call after 500 ms
         self.master.after(10, self.act)
 
@@ -41,10 +63,10 @@ class WasteManagementGUI:
             self.waste_level_text.delete(0, tk.END)
             self.waste_level_text.insert(0, str(level) + "%")
             self.waste_level_text.config(state="disabled")
-            if level >= 80:
-                self.status_label.config(text="Status: Turned On", foreground="green")
-            else:
-                self.status_label.config(text="Status: Turned Off", foreground="red")
+            #if level >= 80:
+            #    self.status_label.config(text="Status: Turned On", foreground="green")
+            #else:
+            #    self.status_label.config(text="Status: Turned Off", foreground="red")
         except ValueError:
             pass
 
--- Final_Project_IOT/test_oop.py
+++ Final_Project_IOT/test_oop.py
@@ -35,9 +35,12 @@ class interactions:
     module = distance_module_object.distance_module() # the distance module object
     bar = led_bar_object.led_bar() # the led bar object
     security = security_sequence.security_system() # The security System
+    percentage = 0
 
-    opened = False
+    opened = False # true if the can is opened
+    unothorized_entry = False # true if the can was opened without authorization
 
+    # GPIO setup for the buzzer
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(18, GPIO.OUT)
 
@@ -45,10 +48,16 @@ class interactions:
     def flip_percentage(self, x):
         return 100 - x
 
+    # method that checks the distance, set the percentage, and buzz the buzzer upon unothorized entry
     def act(self):
         try:
-            dist = self.module.check_distance()
+            # gets the distance from the distance module
+            dist = self.module.check_distance() 
             print("distance: ", dist)
+            
+            # if the distance is greater than 1, the can is considered opened.
+            # we set the distance to 1 (100%) to avoid issues later in the code.
+            # we rearm the security system once the can gets closed again.
             if(dist > 1):
                 self.opened = True
                 dist = 1
@@ -56,16 +65,31 @@ class interactions:
                 if(self.opened):
                     self.security.set_openable_false()
                 self.opened = False
-            self.bar.set_percentage(self.flip_percentage(dist * 100))
+                
+            # turns the distance into a percentage and flips the percentage
+            # (closer to the sensor is a higher persentage)
+            self.percentage = self.flip_percentage(dist * 100)
+            self.bar.set_percentage(self.percentage)
         
+            # sounds the buzzer upon unothorized entry
             if(not self.security.get_openable() and self.opened):
                 GPIO.output(18, GPIO.HIGH)
+                self.unothorized_entry = True
             else:
                 GPIO.output(18, GPIO.LOW)
-        
+                unothorized_entry = False
             if(not self.opened):
                 GPIO.output(18, GPIO.LOW)
+                unothorized_entry = False
             
         except KeyboardInterrupt:
             GPIO.cleanup()
             exit()
+          
+    # returns the current percentage for the UI
+    def get_percentage(self):
+        return self.percentage
+    
+    def get_authorization(self):
+        print(self.unothorized_entry)
+        return self.unothorized_entry
